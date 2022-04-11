82f4ae8fb06049f5a0e80db57fa0a844e9a35057
Danylo Kondratiev <knightpp@protonmail.com>
2022-04-11 17:44:27 +0300

    Add update nix script

---
+++ dot_local/bin/executable_update-nix-packages.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+
+set -x
+set -e
+
+nix profile list | awk '{print $4}' > /tmp/old && \
+    nix profile upgrade --impure '.*' && \
+    nix profile list | awk '{print $4}' > /tmp/new && \
+    meld /tmp/old /tmp/new
