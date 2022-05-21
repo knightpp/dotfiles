b0906df5a061c0370e2502a41074f68fd38d61cf
Danil Kondratiev <knightpp@protonmail.com>
2022-05-21 13:18:23 +0300

    Update dot_local/bin/executable_shutdown-update.sh

--- dot_local/bin/executable_shutdown-update.sh
+++ dot_local/bin/executable_shutdown-update.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-sudo pacman -Syu --noconfirm
-flatpak update --assumeyes
-shutdown -h now
+printf "=========== START %s ===========\n" "$(date)" >>~/pacman-updates.log
+sudo pacman -Syu --noconfirm | tee --append ~/pacman-updates.log
+printf "=========== END   %s ===========\n\n" "$(date)" >>~/pacman-updates.log
+
+printf "=========== START %s ===========\n" "$(date)" >>~/flatpak-updates.log
+flatpak update --assumeyes --noninteractive | tee --append ~/flatpak-updates.log
+printf "=========== END   %s ===========\n\n" "$(date)" >>~/flatpak-updates.log
+
+qdbus org.kde.ksmserver /KSMServer logout 0 2 2
+#shutdown -h now
