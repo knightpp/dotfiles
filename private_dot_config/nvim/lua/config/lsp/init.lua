113f2ca02bf657bf2a54b9b581514ad78801f213
Danil Kondratiev <knightpp@protonmail.com>
2022-05-30 23:54:13 +0300

    Update private_dot_config/nvim/lua/config/lsp/init.lua

--- private_dot_config/nvim/lua/config/lsp/init.lua
+++ private_dot_config/nvim/lua/config/lsp/init.lua
@@ -1,8 +1,9 @@
 local M = {}
-
+local vim = vim
 local servers = {
 	gopls = {},
 	sumneko_lua = {},
+	pyright = {},
 }
 
 --local lsp_signature = require "lsp_signature"
@@ -20,11 +21,8 @@ local function on_attach(client, bufnr)
 	require("config.lsp.keymaps").setup(client, bufnr)
 end
 
---local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()) -- for nvim-cmp
-
 local opts = {
 	on_attach = on_attach,
---	capabilities = capabilities,
 	flags = {
 		debounce_text_changes = 150,
 	},
