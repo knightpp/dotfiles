b7091ee782003a2b196a27a7163a1d7f2f658bbd
Danylo Kondratiev <knightpp@protonmail.com>
2022-08-16 10:05:26 +0300

    Update dot_local/bin/executable_update-nix-packages.sh

--- dot_local/bin/executable_update-nix-packages.sh
+++ dot_local/bin/executable_update-nix-packages.sh
@@ -3,7 +3,8 @@
 set -x
 set -e
 
+    #nix profile upgrade --impure '.*' && \
 nix profile list | awk '{print $4}' > /tmp/old && \
-    nix profile upgrade --impure '.*' && \
+    nix profile upgrade '.*' && \
     nix profile list | awk '{print $4}' > /tmp/new && \
     meld /tmp/old /tmp/new
